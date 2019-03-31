54fdc4e4b93a9c0133ec85c5ffafc67ce3ada41b
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-03-30 22:04:46 -0400

    Make App component stateful via useState

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
-const App = () => {
+const { useState } = React;
+
+const data = {
+  quotes: [
+    'Two possibilities exist: Either we are alone in the Universe or we are not. Both are equally terrifying.',
+  ],
+};
+
+const App = ({ data }) => {
+  const [quotes, setQuotes] = useState(data.quotes);
+  const quoteElems = quotes.map((quote) => <section><blockquote>{ quote }</blockquote></section>);
   return (
-    <h1>Hello, world!</h1>
+    <main>{ quoteElems }</main>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App data={ data } />, document.getElementById('root'));
